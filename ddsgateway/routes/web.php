1213af46d8d17e04d77f95d52d03eaea046340cb
damiotanjayson <damiotanjayson2906@gmail.com>
2020-12-06 22:44:21 +0800

    06/12/20 Final Commit

--- ddsgateway/routes/web.php
+++ ddsgateway/routes/web.php
@@ -17,16 +17,18 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/users1','user1Controller@displayUser'); //get all user from ddsbe
-$router->post('/users1','user1Controller@addUser'); //add user to ddsbe
-$router->get('/users1/{id}', 'user1Controller@readUser'); // get user by id from ddsbe
-$router->put('/users1/{id}','user1Controller@updateUser'); // update user record from ddsbe
-$router->delete('/users1/{id}', 'user1Controller@deleteUser'); //delete record  from ddsbe
+$router->group(['middleware'=>'client.credentials'],function() use ($router){
 
-$router->get('/users2','user2Controller@displayUser'); //get all user from ddsbe
-$router->post('/users2','user2Controller@addUser'); //add user to ddsbe
-$router->get('/users2/{id}', 'user2Controller@readUser'); // get user by id from ddsbe
-$router->put('/users2/{id}','user2Controller@updateUser'); // update user record from ddsbe
-$router->delete('/users2/{id}', 'user2Controller@deleteUser'); //delete record  from ddsbe
+    $router->get('/users1','user1Controller@displayUser'); //get all user from ddsbe
+    $router->post('/users1','user1Controller@addUser'); //add user to ddsbe
+    $router->get('/users1/{id}', 'user1Controller@readUser'); // get user by id from ddsbe
+    $router->put('/users1/{id}','user1Controller@updateUser'); // update user record from ddsbe
+    $router->delete('/users1/{id}', 'user1Controller@deleteUser'); //delete record  from ddsbe
 
+    $router->get('/users2','user2Controller@displayUser'); //get all user from ddsbe
+    $router->post('/users2','user2Controller@addUser'); //add user to ddsbe
+    $router->get('/users2/{id}', 'user2Controller@readUser'); // get user by id from ddsbe
+    $router->put('/users2/{id}','user2Controller@updateUser'); // update user record from ddsbe
+    $router->delete('/users2/{id}', 'user2Controller@deleteUser'); //delete record  from ddsbe
 
+});
